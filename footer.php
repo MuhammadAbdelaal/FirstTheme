7629e254f890ece0ec2fa0e83d8938f5fd15da3d
MuhammadAbdelaal <dev.mureda@gmail.com>
2020-08-30 13:22:24 +0200

    posts loop and custom body classes

--- footer.php
+++ footer.php
@@ -1,8 +1,9 @@
         <footer>
+        <hr>
             
             <p>This is my footer</p>
             <?php wp_nav_menu(array('theme_location'=>'secondry')); ?> <!-- Display footer menu -->
-            
+
         </footer>
         <?php wp_footer(); ?>
     </body>
--- header.php
+++ header.php
@@ -6,8 +6,21 @@
     <title>First Theme</title>
     <?php wp_head(); ?>
 </head>
-    <body>
+
+    <?php
+
+        if( is_front_page()): //detect if this is the custom home page / the front page
+
+            $first_body_classes = array('home-page', 'front-page'); //custom css classes
+
+        endif;
+    ?>
+
+    <body <?php body_class( $first_body_classes); ?>>
     
         <?php wp_nav_menu(array('theme_location'=>'primary')); ?> <!-- Display header menu -->
+        
         <p>This is my header</p>
+        
+        <hr>
     
