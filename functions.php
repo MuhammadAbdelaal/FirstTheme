1c46fc8d3c5c5d893d375590c8484733a405b665
MuhammadAbdelaal <dev.mureda@gmail.com>
2020-08-30 11:38:20 +0200

    starter files

---
+++ functions.php
@@ -0,0 +1,13 @@
+<?php
+
+function first_script_enqueue() {
+    
+    // inculde our custom css file
+    wp_enqueue_style( 'customstyle', get_template_directory_uri() . '/css/first.css', array(), '1.0.0', 'all');
+
+    // inculde our custom css file
+    wp_enqueue_script( "customjs", get_template_directory_uri() . '/js/first.js', array(), '1.0.0', true);
+
+}
+
+add_action("wp_enqueue_scripts", "first_script_enqueue");
